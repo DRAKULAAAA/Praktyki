ee4994b8515bcbfe547d2bca352602e1cc303c7c
Oliwier Nalewajek <149157312+DantEltk@users.noreply.github.com>
2024-03-22 14:18:21 +0100

    Add files via upload

--- sklep/rejestAdmin.php
+++ sklep/rejestAdmin.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
     header("Location: administracja.php");
     exit();
@@ -9,15 +8,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['login'];
     $password = $_POST['haslo'];
     $db = mysqli_connect('localhost','root','','sklep');
-    $query = "SELECT * FROM dane WHERE login='$username' AND haslo='$password'";
-    $result = mysqli_query($db, $query);
-    $count = mysqli_num_rows($result);
-    if($count == 1) {
+    if (!$db) {
+        die("połączenie nie powiodło się: " . mysqli_connect_error());
+    }
+    $query = "INSERT INTO admin1 (login, haslo) VALUES ('$username', '$password')";
+    if(mysqli_query($db, $query)) {
         $_SESSION['logged_in'] = true;
         $_SESSION['login'] = $username;
-        header("Location:  administracja.php");
+        header("Location: administracja.php");
+        exit();
     } else {
-        $error = "Błędny login lub hasło";
+        echo "Error: " . $query . "<br>" . mysqli_error($db);
     }
 }
 ?>
@@ -26,18 +27,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style5.css">
-    <title>Logowanie</title>
+    <link rel="stylesheet" href="style6.css">
+    <title>Rejestracja</title>
 </head>
 <body>
-    <header><h2>Logowanie</h2></header>
-    
+<header><h2>Rejestracja</h2></header>
     <form method="post" action="">
         <label for="login">Nazwa użytkownika:</label><br>
         <input type="text" id="login" name="login" required><br>
         <label for="haslo">Hasło:</label><br>
         <input type="password" id="haslo" name="haslo" required><br><br>
-        <input type="submit" value="Zaloguj">
+        <input type="submit" value="Zarejestruj">
     </form>
 </body>
 </html>
