4b07aa6928fd17200cfb5b69d10bdc1e77655297
Oliwier Nalewajek <149157312+DantEltk@users.noreply.github.com>
2024-03-27 14:42:33 +0100

    Update Repeats.php

--- lotto/Repeats.php
+++ lotto/Repeats.php
@@ -4,9 +4,12 @@ $connection = mysqli_connect('localhost', 'root', '', 'lotto');
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 $repeats = [];
 
-for ($i = 1; $i <= 49; $i++) {
+echo "<h1>Pomyślnie obliczono i zapisano liczbę powtórzeń w bazie danych.</h1>";
+
+for ($i = 1; $i <= 25; $i++) {
     $query = "SELECT COUNT(*) AS count FROM results WHERE $i IN (n1, n2, n3, n4, n5, n6)";
     $result = mysqli_query($connection, $query);
     $row = mysqli_fetch_assoc($result);
@@ -14,8 +17,8 @@ for ($i = 1; $i <= 49; $i++) {
 
     $insertQuery = "INSERT INTO repeats (number, repeats) VALUES ($i, $repeats[$i])";
     mysqli_query($connection, $insertQuery);
-}
-echo "<h1>Pomyślnie obliczono i zapisano liczbę powtórzeń w bazie danych.</h1>";
 
+    echo "Liczba $i wystąpiła $repeats[$i] razy.<br>";
+}
 mysqli_close($connection);
 ?>
