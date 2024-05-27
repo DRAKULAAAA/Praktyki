c890f9e1d24daa200d31af2a306e40bc12c312c1
Oliwier Nalewajek <149157312+DantEltk@users.noreply.github.com>
2024-05-27 11:06:34 +0200

    Add files via upload

---
+++ xd/futbol.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Rozgrywki futbolowe</title>
+</head>
+<body>
+    <header>
+        <h2>Światowe rozgrywki piłkarskie</h2>
+        <img src="obraz1.jpg" alt="boisko">
+    </header>
+
+    <section class="mecze">
+        
+    </section>
+
+    <main>
+        <h2>Reprezentacja Polski</h2>
+    </main>
+
+    <aside class="lewy">
+        <p>Podaj pozycję zawpdników 
+           (1-bramkarze, 2-obrońcy, 3-pomocnicy, 4-napastnicy):
+        </p>
+
+        <form>
+            <input type="number">
+            <button>Sprawdź</button>
+        </form>
+    </aside>
+
+    <aside class="prawy">
+        <img src="zad1.png" alt="piłkarz">
+        <p>Autor: 00000000</p>
+    </aside>
+</body>
+</html>
